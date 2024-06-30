b024ba78819af679351e2a9d4898d8738c7fb778
Nsakrty <119097728+Nsakrty@users.noreply.github.com>
2024-06-30 15:36:20 +0800

    Add files via upload

--- generator.js
+++ generator.js
@@ -1,10 +1,19 @@
 function generate(planetData) {
   const planetStyle = document.getElementById("result").style;
-  planetStyle.setProperty("--planetColor", planetData.color);
-  planetStyle.setProperty("--planetRadiusPercent", planetData.radiusPercent);
-  planetStyle.setProperty("--planetAtmosphereSize", planetData.atmosphere.size);
-  planetStyle.setProperty("--planetCloudRotate", planetData.cloud.rotate);
-  planetStyle.setProperty("--planetCloudOpacity", planetData.cloud.opacity);
+  const table = [
+    ["--planetColor", planetData.color],
+    ["--planetRadiusPercent", planetData.radiusPercent],
+    ["--planetAtmosphereSize", planetData.atmosphere.size],
+    ["--planetCloudRotate", planetData.cloud.rotate],
+    ["--planetCloudOpacity", planetData.cloud.opacity],
+    ["--planetDetailColor", planetData.detail.color],
+    ["--planetDetailOpacity", planetData.detail.opacity],
+    ["--planetDetailRotate", planetData.detail.rotate],
+  ];
+  table.forEach((item) => {
+    planetStyle.setProperty(...item);
+  });
+  console.log(planetData);
 }
 function randomGenerate() {
   let planetColor = randomColor();
@@ -14,10 +23,15 @@ function randomGenerate() {
     atmosphere: {
       size: `${randomInRange(0, 20)}px`,
     },
-    cloud:{
+    cloud: {
       rotate: `${randomInRange(0, 360)}deg`,
-      opacity: Math.random()
-    }
+      opacity: Math.random(),
+    },
+    detail: {
+      color: `${randomInRange(0, 360)}deg`,
+      opacity: Math.random() / 2,
+      rotate: `${randomInRange(0, 360)}deg`,
+    },
   };
   generate(planetData);
 }
