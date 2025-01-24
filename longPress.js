5c98445081edd9c82faf16b851f4a95e66733c9a
Nsakrty <Nsakrty@163.com>
2025-01-25 00:19:55 +0800

    feat(longPress.js): prevent text selection on long press

--- longPress.js
+++ longPress.js
@@ -2,7 +2,9 @@ let delayTime = 100
  function longPress(element, callback) {
   let timer;
   let isPressed = false;
-
+  element.addEventListener("selectStart",()=>{
+    element.preventDefault()
+  })
   element.addEventListener("mousedown", () => {
     isPressed = true;
     callback()
