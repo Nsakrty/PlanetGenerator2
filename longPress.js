56e0afc8e66ed4bc939eca0973212f3ef4468682
Nsakrty <Nsakrty@163.com>
2025-01-25 00:04:30 +0800

    feat: fix camera controls and long press functionality

---
+++ longPress.js
@@ -0,0 +1,33 @@
+let delayTime = 100
+ function longPress(element, callback) {
+  let timer;
+  let isPressed = false;
+
+  element.addEventListener("mousedown", () => {
+    isPressed = true;
+    callback()
+    timer = setInterval(callback, delayTime); 
+  });
+
+  element.addEventListener("mouseup", () => {
+    isPressed = false;
+    clearInterval(timer);
+  });
+
+  element.addEventListener("mouseleave", () => {
+    isPressed = false;
+    clearInterval(timer);
+  });
+
+  element.addEventListener("touchstart",()=>{
+    isPressed = true;
+    callback();
+    timer = setInterval(callback, delayTime); 
+    element.preventDefault()
+  }
+  );
+  element.addEventListener("touchend",()=>{
+    isPressed = false;
+    clearInterval(timer);
+  })
+}
