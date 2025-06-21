fdce57f944e9cb65eb2b527a999cff0a59b19899
Nsakrty <Nsakrty@163.com>
2025-06-21 12:36:42 +0800

    feat: enhance planet rotation with offset calculation

--- generator.js
+++ generator.js
@@ -110,7 +110,7 @@ function randomGenerate(onlyRandomData = false) {
     type: randomItem(planetType),
     color: randomColor(),
     radiusPercent: `${Math.max(0.1, normalRandom(0.4, 0.1))}`,
-    rotate: `${randomInRange(0, 360)}deg`,
+    rotate: `calc(${randomInRange(0, 360)}deg + var(--planetRotateOffset))`,
     atmosphere: {
       size: `${randomInRange(0, 20)}px`,
     },
